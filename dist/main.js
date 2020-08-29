d712a5c064b5ee0e4b2ae458e0593ffd754b3a30
jeason1997 <568553473@qq.com>
2020-08-29 15:37:49 +0000

    添加DEBUG

--- dist/main.js
+++ dist/main.js
@@ -9,18 +9,40 @@ var Main = /** @class */ (function () {
     }
     Main.prototype.run = function () {
         var d = new rot_js_1.Display({
-            width: 80,
-            height: 30,
             layout: WEB ? 'rect' : 'term',
         });
         if (WEB) {
+            d._options.spacing = 1.1;
             document.body.appendChild(d.getContainer());
             //d._options.width = document.body.clientHeight / d._options.fontSize;
             //d._options.height = document.body.clientWidth / d._options.fontSize;
         }
         else {
-            //d._options.width = process.stdout.rows;
-            //d._options.height = process.stdout.columns;
+            d._options.width = process.stdout.columns;
+            d._options.height = process.stdout.rows;
+            // acquire some libraries we'll need
+            var keypress = require("keypress");
+            // when the program terminates, put the console back the way we found it
+            process.on("exit", function () {
+                // move cursor to the bottom left corner
+                process.stdout.write("\x1b[" + (process.stdout.rows + 1) + ";1H");
+                // show the cursor again
+                process.stdout.write("\x1b[?25h");
+            });
+            // during the game, hide the cursor from display
+            process.stdout.write("\x1b[?25l");
+            // put the keyboard into raw mode, so we can get individual keypress events
+            keypress(process.stdin);
+            process.stdin.setRawMode(true);
+            process.stdin.resume();
+            // add a handler to listen for "quit game" commands
+            process.stdin.on("keypress", function (ch, key) {
+                // if the user pressed Ctrl+C or ESC
+                if (ch === "\u0003" || ch === "\u001b") {
+                    // then quit the game
+                    process.exit(0);
+                }
+            });
         }
         for (var i = 0; i < d._options.width; i++) {
             for (var j = 0; j < d._options.height; j++) {
@@ -37,7 +59,8 @@ var Main = /** @class */ (function () {
             var fg = rot_js_2.Color.toRGB([i * t, i * t, i * t]);
             var bg = rot_js_2.Color.toRGB([255 - i * t, 255 - i * t, 255 - i * t]);
             //d.drawText(0, i, `%c{${fg}}%b{${bg}}Hello ${i}`);
-            d.draw(0, i, ['`', '.'], 'yellow', 'black');
+            d.drawText(0, i, '%c{rgb(255, 255, 0)}HELLO' + i.toString());
+            d.draw(i, 0, '@', fg, bg);
         }
         d.draw(d._options.width >> 1, d._options.height >> 1, "@", "goldenrod", "blakc");
     };
--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,47 @@ export class Main {
 
 	run() {
 		let d = new Display({
-			width: 80,
-			height: 30,
 			layout: WEB ? 'rect' : 'term',
 			//bg: "gray",
 		});
 
 		if (WEB) {
+			d._options.spacing = 1.1;
+			
 			document.body.appendChild(d.getContainer() as Node);
 			//d._options.width = document.body.clientHeight / d._options.fontSize;
 			//d._options.height = document.body.clientWidth / d._options.fontSize;
 		}
 		else {
-			//d._options.width = process.stdout.rows;
-			//d._options.height = process.stdout.columns;
+			d._options.width = process.stdout.columns;
+			d._options.height = process.stdout.rows;
+
+			// acquire some libraries we'll need
+			var keypress = require("keypress");
+			
+			// when the program terminates, put the console back the way we found it
+			process.on("exit", function () {
+				// move cursor to the bottom left corner
+				process.stdout.write("\x1b[" + (process.stdout.rows + 1) + ";1H");
+				// show the cursor again
+				process.stdout.write("\x1b[?25h");
+			});
+			// during the game, hide the cursor from display
+			process.stdout.write("\x1b[?25l");
+
+			// put the keyboard into raw mode, so we can get individual keypress events
+			keypress(process.stdin);
+			process.stdin.setRawMode(true);
+			process.stdin.resume();
+
+			// add a handler to listen for "quit game" commands
+			process.stdin.on("keypress", function (ch, key) {
+				// if the user pressed Ctrl+C or ESC
+				if (ch === "\u0003" || ch === "\u001b") {
+					// then quit the game
+					process.exit(0);
+				}
+			});
 		}
 
 		for (let i = 0; i < d._options.width; i++) {
@@ -29,7 +56,7 @@ export class Main {
 				if (!i || !j || i + 1 == d._options.width || j + 1 == d._options.height) {
 					d.draw(i, j, "#", "black", "gray");
 				}
-				else{
+				else {
 					d.draw(i, j, ".", "#666", "black");
 				}
 			}
@@ -40,7 +67,8 @@ export class Main {
 			let fg = Color.toRGB([i * t, i * t, i * t]);
 			let bg = Color.toRGB([255 - i * t, 255 - i * t, 255 - i * t]);
 			//d.drawText(0, i, `%c{${fg}}%b{${bg}}Hello ${i}`);
-			d.draw(0, i, ['`', '.'], 'yellow', 'black');
+			d.drawText(0, i, '%c{rgb(255, 255, 0)}HELLO' + i.toString());
+			d.draw(i, 0, '@', fg, bg);
 		}
 
 
